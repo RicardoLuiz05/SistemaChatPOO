4650d5158071e6e984e2f37017c1c84c9108f3ea
Juliana Ferreira <julianafercavalcante@hotmail.com>
2023-07-02 02:53:39 -0300

    aplicação swing

--- src/appswing/TelaEspionagem.java
+++ src/appswing/TelaEspionagem.java
@@ -8,18 +8,16 @@ package appswing;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.TrayIcon.MessageType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -31,20 +29,19 @@ import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
 import modelo.Mensagem;
-import modelo.Participante;
 import regras_negocio.Fachada;
 
-public class TelaConversa {
+public class TelaEspionagem {
 	private JFrame frame;
 	private JTable table;
 	private JScrollPane scrollPane;
 	private JButton button;
-	private JButton button_4;
 	private JLabel label;
-	private JTextField textField;
+	private ButtonGroup grupobotoes;
 
-	private JComboBox<String> comboBox;
+	private JTextField textField_2;
 	private JLabel label_1;
+	private JButton button_1;
 
 	/**
 	 * Launch the application.
@@ -65,7 +62,7 @@ public class TelaConversa {
 	/**
 	 * Create the application.
 	 */
-	public TelaConversa() {
+	public TelaEspionagem() {
 		initialize();
 	}
 
@@ -79,16 +76,16 @@ public class TelaConversa {
 			@Override
 			public void windowOpened(WindowEvent e) {
 				frame.setTitle(frame.getTitle() + " -- logado:" + TelaPrincipal.logado.getNome());
-				carregarComboBox();
+				listagem();
 			}
 		});
-		frame.setTitle("Conversa");
-		frame.setBounds(100, 100, 851, 411);
+		frame.setTitle("Espionagem");
+		frame.setBounds(100, 100, 819, 411);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(24, 53, 785, 179);
+		scrollPane.setBounds(24, 53, 749, 283);
 		frame.getContentPane().add(scrollPane);
 
 		table = new JTable();
@@ -106,106 +103,55 @@ public class TelaConversa {
 		table.setShowGrid(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
-		button_4 = new JButton("Apagar mensagem");
-		button_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					if (table.getSelectedRow() < 0)
-						JOptionPane.showMessageDialog(null, "selecione uma linha");
-					else {
-						// pegar o id na linha selecionada
-						int id = (int) table.getValueAt(table.getSelectedRow(), 0); // 0=nome
-
-						Object[] options = { "Confirmar", "Cancelar" };
-						int escolha = JOptionPane.showOptionDialog(null, "Esta opera��o apagar� a mensagem", "Alerta",
-								JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-						if (escolha == 0) {
-							Fachada.apagarMensagem(TelaPrincipal.logado.getNome(), id);
-							listagem();
-							label.setText("exclus�o realizada");
-						} else
-							label.setText("exclus�o cancelada");
-					}
-				} catch (Exception ex) {
-					label.setText(ex.getMessage());
-				}
-			}
-		});
-		button_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button_4.setBounds(24, 277, 176, 23);
-		frame.getContentPane().add(button_4);
-
 		label = new JLabel("");
 		label.setForeground(Color.RED);
 		label.setBounds(21, 347, 718, 14);
 		frame.getContentPane().add(label);
 
-		button = new JButton("Criar mensagem");
+		button = new JButton("listar ausentes");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					int index = comboBox.getSelectedIndex();
-					if (index < 0)
-						label.setText("selecione um destinatario");
-					else {
-						String nome = (String) comboBox.getSelectedItem();
-						Fachada.criarMensagem(TelaPrincipal.logado.getNome(), nome, textField.getText());
-						listagem();
-					}
+					ArrayList<String> lista = Fachada.ausentes(TelaPrincipal.logado.getNome());
+					String texto = "";
+					for (String s : lista)
+						texto += s + "\n";
+					JOptionPane.showMessageDialog(null, texto, "Ausentes", JOptionPane.PLAIN_MESSAGE);
 				} catch (Exception ex) {
 					label.setText(ex.getMessage());
 				}
 			}
 		});
 		button.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button.setBounds(21, 243, 176, 23);
+		button.setBounds(518, 19, 161, 23);
 		frame.getContentPane().add(button);
 
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		textField.setColumns(10);
-		textField.setBounds(207, 244, 602, 20);
-		frame.getContentPane().add(textField);
+		grupobotoes = new ButtonGroup();
 
-		comboBox = new JComboBox<>();
-		comboBox.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
+		textField_2 = new JTextField();
+		textField_2.setBounds(96, 22, 161, 20);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+
+		label_1 = new JLabel("pesquisa:");
+		label_1.setBounds(24, 25, 70, 14);
+		frame.getContentPane().add(label_1);
+
+		button_1 = new JButton("Listar");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				listagem();
 			}
 		});
-		comboBox.setBounds(184, 20, 301, 22);
-		frame.getContentPane().add(comboBox);
-
-		label_1 = new JLabel("Selecione o destinatario:");
-		label_1.setBounds(29, 24, 145, 14);
-		frame.getContentPane().add(label_1);
+		button_1.setBounds(278, 20, 89, 23);
+		frame.getContentPane().add(button_1);
 
 		frame.setVisible(true);
 	}
 
-	public void carregarComboBox() {
-		List<Participante> todos = new ArrayList<>();
-		todos.addAll(Fachada.listarIndividuos());
-		todos.addAll(Fachada.listarGrupos());
-
-		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
-		for (Participante p : todos) {
-			model.addElement(p.getNome());
-		}
-		comboBox.setModel(model);
-		comboBox.setSelectedIndex(-1);
-	}
-
-	public void fechar() {
-		frame.dispose();
-	}
-
 	public void listagem() {
 		try {
-			if (comboBox.getSelectedIndex() < 0)
-				return;
-			String destinatario = (String) comboBox.getSelectedItem();
-			List<Mensagem> lista = Fachada.obterConversa(TelaPrincipal.logado.getNome(), destinatario);
+			List<Mensagem> lista = Fachada.espionarMensagens(TelaPrincipal.logado.getNome(), textField_2.getText());
 
 			// objeto model contem todas as linhas e colunas da tabela
 			DefaultTableModel model = new DefaultTableModel();
@@ -224,7 +170,7 @@ public class TelaConversa {
 						m.getEmitente().getNome(), 
 						m.getDestinatario().getNome(),
 						m.getTexto() 
-						});
+						});			
 				}
 			table.setModel(model);
 
@@ -237,5 +183,9 @@ public class TelaConversa {
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(frame, ex.getMessage());
 		}
+	}
+
+	public void fechar() {
+		frame.dispose();
 	}
 }
--- src/appswing/TelaMensagem.java
+++ src/appswing/TelaMensagem.java
@@ -8,43 +8,41 @@ package appswing;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.border.EtchedBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
 import modelo.Mensagem;
-import modelo.Participante;
 import regras_negocio.Fachada;
 
-public class TelaConversa {
+public class TelaMensagem {
 	private JFrame frame;
 	private JTable table;
 	private JScrollPane scrollPane;
-	private JButton button;
-	private JButton button_4;
 	private JLabel label;
-	private JTextField textField;
-
-	private JComboBox<String> comboBox;
-	private JLabel label_1;
+	private ButtonGroup grupobotoes;
+	private JButton button_1;
+	private JPanel panel;
+	private JRadioButton radioButton;
+	private JRadioButton radioButton_1;
 
 	/**
 	 * Launch the application.
@@ -65,7 +63,7 @@ public class TelaConversa {
 	/**
 	 * Create the application.
 	 */
-	public TelaConversa() {
+	public TelaMensagem() {
 		initialize();
 	}
 
@@ -79,16 +77,16 @@ public class TelaConversa {
 			@Override
 			public void windowOpened(WindowEvent e) {
 				frame.setTitle(frame.getTitle() + " -- logado:" + TelaPrincipal.logado.getNome());
-				carregarComboBox();
+				listagem();
 			}
 		});
-		frame.setTitle("Conversa");
-		frame.setBounds(100, 100, 851, 411);
+		frame.setTitle("Mensagens");
+		frame.setBounds(100, 100, 838, 359);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(24, 53, 785, 179);
+		scrollPane.setBounds(24, 53, 758, 218);
 		frame.getContentPane().add(scrollPane);
 
 		table = new JTable();
@@ -100,112 +98,58 @@ public class TelaConversa {
 		table.setFillsViewportHeight(true);
 		table.setRowSelectionAllowed(true);
 		table.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		scrollPane.setViewportView(table);
+		scrollPane.setColumnHeaderView(table);
 		table.setBorder(new LineBorder(new Color(0, 0, 0)));
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setShowGrid(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-
-		button_4 = new JButton("Apagar mensagem");
-		button_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					if (table.getSelectedRow() < 0)
-						JOptionPane.showMessageDialog(null, "selecione uma linha");
-					else {
-						// pegar o id na linha selecionada
-						int id = (int) table.getValueAt(table.getSelectedRow(), 0); // 0=nome
-
-						Object[] options = { "Confirmar", "Cancelar" };
-						int escolha = JOptionPane.showOptionDialog(null, "Esta opera��o apagar� a mensagem", "Alerta",
-								JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-						if (escolha == 0) {
-							Fachada.apagarMensagem(TelaPrincipal.logado.getNome(), id);
-							listagem();
-							label.setText("exclus�o realizada");
-						} else
-							label.setText("exclus�o cancelada");
-					}
-				} catch (Exception ex) {
-					label.setText(ex.getMessage());
-				}
-			}
-		});
-		button_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button_4.setBounds(24, 277, 176, 23);
-		frame.getContentPane().add(button_4);
 
 		label = new JLabel("");
 		label.setForeground(Color.RED);
 		label.setBounds(21, 347, 718, 14);
 		frame.getContentPane().add(label);
 
-		button = new JButton("Criar mensagem");
-		button.addActionListener(new ActionListener() {
+		grupobotoes = new ButtonGroup();
+
+		button_1 = new JButton("Listar");
+		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					int index = comboBox.getSelectedIndex();
-					if (index < 0)
-						label.setText("selecione um destinatario");
-					else {
-						String nome = (String) comboBox.getSelectedItem();
-						Fachada.criarMensagem(TelaPrincipal.logado.getNome(), nome, textField.getText());
-						listagem();
-					}
-				} catch (Exception ex) {
-					label.setText(ex.getMessage());
-				}
-			}
-		});
-		button.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		button.setBounds(21, 243, 176, 23);
-		frame.getContentPane().add(button);
-
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		textField.setColumns(10);
-		textField.setBounds(207, 244, 602, 20);
-		frame.getContentPane().add(textField);
-
-		comboBox = new JComboBox<>();
-		comboBox.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
 				listagem();
 			}
 		});
-		comboBox.setBounds(184, 20, 301, 22);
-		frame.getContentPane().add(comboBox);
+		button_1.setBounds(256, 19, 89, 23);
+		frame.getContentPane().add(button_1);
 
-		label_1 = new JLabel("Selecione o destinatario:");
-		label_1.setBounds(29, 24, 145, 14);
-		frame.getContentPane().add(label_1);
+		panel = new JPanel();
+		panel.setBorder(new TitledBorder(
+				new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Tipo",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(24, 11, 222, 37);
+		frame.getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 2));
 
+		radioButton = new JRadioButton("recebidas");
+		radioButton.setSelected(true);
+		panel.add(radioButton);
+
+		radioButton_1 = new JRadioButton("enviadas");
+		radioButton_1.setSelected(true);
+		panel.add(radioButton_1);
+
+		grupobotoes = new ButtonGroup();	//permite selecao unica dos botoes
+		grupobotoes.add(radioButton);
+		grupobotoes.add(radioButton_1);
+		
 		frame.setVisible(true);
-	}
-
-	public void carregarComboBox() {
-		List<Participante> todos = new ArrayList<>();
-		todos.addAll(Fachada.listarIndividuos());
-		todos.addAll(Fachada.listarGrupos());
-
-		DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
-		for (Participante p : todos) {
-			model.addElement(p.getNome());
-		}
-		comboBox.setModel(model);
-		comboBox.setSelectedIndex(-1);
-	}
-
-	public void fechar() {
-		frame.dispose();
 	}
 
 	public void listagem() {
 		try {
-			if (comboBox.getSelectedIndex() < 0)
-				return;
-			String destinatario = (String) comboBox.getSelectedItem();
-			List<Mensagem> lista = Fachada.obterConversa(TelaPrincipal.logado.getNome(), destinatario);
+			List<Mensagem> lista;
+			if (radioButton.isSelected())
+				lista = Fachada.listarMensagensRecebidas(TelaPrincipal.logado.getNome());
+			else
+				lista = Fachada.listarMensagensEnviadas(TelaPrincipal.logado.getNome());
 
 			// objeto model contem todas as linhas e colunas da tabela
 			DefaultTableModel model = new DefaultTableModel();
@@ -217,7 +161,7 @@ public class TelaConversa {
 			model.addColumn("Texto");
 			// criar as linhas da tabela
 			label.setText("");
-			for (Mensagem m : lista) {
+			for(Mensagem m : lista) {
 				model.addRow(new Object[]{
 						m.getId(), 
 						m.getData(),
@@ -225,7 +169,7 @@ public class TelaConversa {
 						m.getDestinatario().getNome(),
 						m.getTexto() 
 						});
-				}
+			}
 			table.setModel(model);
 
 			// redimensionar a coluna 3
@@ -237,5 +181,8 @@ public class TelaConversa {
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(frame, ex.getMessage());
 		}
+	}
+	public void fechar() {
+		frame.dispose();
 	}
 }
